dd8ae76ec270fed18c9fb52a0677768b0be9d32b
EvidenceIsreallaMbagalira <bsc-com-ne-31-24@unima.ac.mw>
2026-03-30 10:18:50 +0200

    Exercise 1 completed

---
+++ Exercise1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0)); // initialize random seed
+
+    int daysUntilExpiration = rand() % 12; // 0–11
+
+    cout << "Days until expiration: " << daysUntilExpiration << endl;
+
+    if (daysUntilExpiration == 0) {
+        cout << "Your subscription has expired." << endl;
+    }
+    else if (daysUntilExpiration == 1) {
+        cout << "Your subscription expires within a day!" << endl;
+        cout << "Renew now and save 20%!" << endl;
+    }
+    else if (daysUntilExpiration <= 5) {
+        cout << "Your subscription expires in " 
+             << daysUntilExpiration << " days." << endl;
+        cout << "Renew now and save 10%!" << endl;
+    }
+    else if (daysUntilExpiration <= 10) {
+        cout << "Your subscription will expire soon. Renew now!" << endl;
+    }
+    else {
+        cout << "You have an active subscription." << endl;
+    }
+
+    return 0;
+}
